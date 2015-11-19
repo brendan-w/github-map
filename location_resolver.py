180e76737a190d2b205fa083c2e8b7b6e28131dd
Brendan Whitfield <brendanw@windworksdesign.com>
2015-11-18 21:33:24 -0500

    moved geonames requests into location_resolver

--- location_resolver.py
+++ location_resolver.py
@@ -1,7 +1,8 @@
 
 import os
-import geonames
+import requests
 from github import Github
+from urllib.parse import urlencode
 
 
 class Location_Resolver:
@@ -11,15 +12,25 @@ class Location_Resolver:
         Export the following env vars to your shell:
             GITHUB_MAP_USERNAME
             GITHUB_MAP_PASSWORD
+            GEONAMES_USERNAME
 
         Eventually, this class may communicate with a datastore
         to avoid excessive GitHub API calls.
     """
-    
+
+    GEONAMES_API_URL = "http://api.geonames.org/search"
+    GEONAMES_PARAMS = {
+        "username": "",
+        "type": "json",
+        "maxRows": 1,
+        "q":"",
+    }
+
     def __init__(self):
         self.cache = {}
         self.gh = Github(os.getenv("GITHUB_MAP_USERNAME", ""), \
                          os.getenv("GITHUB_MAP_PASSWORD", ""))
+        self.GEONAMES_PARAMS["username"] = os.getenv("GEONAMES_USERNAME", "")
 
 
     def __call__(self, username):
@@ -35,8 +46,20 @@ class Location_Resolver:
         if not user.location:
             return None
 
-        return geonames.get_lat_lng(user.location)
+        return self.resolve_location(user.location)
 
+
+    def resolve_location(self, query):
+
+        params = dict(self.GEONAMES_PARAMS)
+        params["q"] = query
+        url = self.GEONAMES_API_URL + "?" + urlencode(params)
+
+        try:
+            r = requests.get(url)
+            return r.json()["geonames"][0]
+        except:
+            return None
 
 
 
