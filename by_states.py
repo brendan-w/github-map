77efbe668ca883389cfa9c8a992c91b1e08919a8
Brendan Whitfield <brendanw@windworksdesign.com>
2015-12-08 17:44:33 -0500

    added by_states

--- by_states.py
+++ by_states.py
@@ -4,7 +4,7 @@ import os
 import sys
 import json
 
-GEOJSON_COUNTRIES = "countries.json"
+GEOJSON_STATES = "states.json"
 
 def main(stats_file, location_file, output_file):
 
@@ -16,8 +16,8 @@ def main(stats_file, location_file, output_file):
         print("location-file '%s' was not found")
         exit(-1)
 
-    with open(GEOJSON_COUNTRIES, "r") as f:
-        countries = json.loads(f.read())
+    with open(GEOJSON_STATES, "r") as f:
+        states = json.loads(f.read())
 
     with open(stats_file, "r") as f:
         stats = json.loads(f.read())
@@ -29,40 +29,42 @@ def main(stats_file, location_file, output_file):
     max_commits = 0
 
     # addition pass
-    for country in countries["features"]:
-        country_code = country["properties"]["ISO_A2"]
-        country["properties"]["commits"] = 0
+    for state in states["features"]:
+        state_name = state["properties"]["NAME"]
+        state["properties"]["commits"] = 0
 
         for stat in stats:
             name = stat["author"]["login"]
 
-            # add up the number of commits per country
+            # add up the number of commits per state
             if locations[name] and \
                "countryCode" in locations[name] and \
-               (locations[name]["countryCode"] == country_code):
-                country["properties"]["commits"] += stat["total"]
+               (locations[name]["countryCode"] == "US") and \
+               locations[name]["adminName1"] == state_name:
 
-        if country["properties"]["commits"] > max_commits:
-            max_commits = country["properties"]["commits"]
+                state["properties"]["commits"] += stat["total"]
+
+        if state["properties"]["commits"] > max_commits:
+            max_commits = state["properties"]["commits"]
 
     # normalization pass
-    for country in countries["features"]:
-        country["properties"]["commits"] /= max_commits
+    for state in states["features"]:
+        state["properties"]["commits"] /= max_commits
 
-    print("max commits for a country: %d" % max_commits)
+    print("max commits for a state: %d" % max_commits)
 
 
 
     # done, write the new map
     with open(output_file, "w") as f:
-        f.write(json.dumps(countries))
+        f.write(json.dumps(states))
 
 
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        print("usage: python by_countries.py <stats-file> <location-file> <output-file>")
+        print("usage: python by_states <stats-file> <location-file> <output-file>")
         exit(-1)
 
     main(sys.argv[1], sys.argv[2], sys.argv[3])
